f139dfba7125e1a1f58f9b128497440b7237a494
Ya-Ching (Jean) Yang <jeanycyang@gmail.com>
2023-06-02 22:14:49 +0800

    fix: 2_gdown script bug

--- 2_gdown_dataset.sh
+++ 2_gdown_dataset.sh
@@ -6,7 +6,7 @@ else
 fi
 
 if [[ -n $RESUME_TRAINING_DATA_UC_ID ]]; then
-  gdown -O /content/dataset.zip https://drive.google.com/uc?id=$RESUME_TRAINING_DATA_UC_ID &
+  gdown -O /content/resume.zip https://drive.google.com/uc?id=$RESUME_TRAINING_DATA_UC_ID &
 else
   echo "RESUME_TRAINING_DATA_UC_ID not given"
 fi
