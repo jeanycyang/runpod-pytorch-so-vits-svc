c9ab23be0a65014825c4a253e937b3840dd532c5
Ya-Ching (Jean) Yang <jeanycyang@gmail.com>
2023-06-03 11:17:27 +0800

    fix: should sync filelists on network storage too

--- 0_if_using_network_storage.sh
+++ 0_if_using_network_storage.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 # volume should be mounted at /network_storage
 cd /content/so-vits-svc
-mkdir -p /network_storage/logs/44k/ /network_storage/logs/44k/diffusion /network_storage/dataset /network_storage/configs
+mkdir -p /network_storage/logs/44k/ /network_storage/logs/44k/diffusion /network_storage/dataset /network_storage/configs /network_storage/filelists
 rm -rf logs
 rm -rf dataset
 rm -rf configs
+rm -rf filelists
 ln -s /network_storage/logs logs
 ln -s /network_storage/dataset dataset
 ln -s /network_storage/configs configs
+ln -s /network_storage/filelists filelists
